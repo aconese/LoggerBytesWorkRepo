6d8e14426231b07108eccac4c6d4b2abca54fe3a
Benjamin Imhof <benjamin.imhof@bytesatwork.ch>
2018-11-09 08:14:39 +0100

    Implement autostart
    Signed-off-by: Benjamin Imhof <benjamin.imhof@bytesatwork.ch>

---
+++ app/src/main/java/io/bytesatwork/skycell/BootCompletedReceiver.java
@@ -0,0 +1,30 @@
+package io.bytesatwork.skycell;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+    private static final String TAG = BootCompletedReceiver.class.getSimpleName();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG+":"+Utils.getLineNumber(), "onReceive");
+
+        //Start Service
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) { //Android 8.0 SDK 26
+            context.startForegroundService(new Intent(context, SkycellService.class));
+        } else {
+            context.startService(new Intent(context, SkycellService.class)
+                .addFlags(Intent.FLAG_RECEIVER_FOREGROUND));
+        }
+
+        //Start Activity
+        context.startActivity(new Intent(context, MainActivity.class)
+            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+
+
+    }
+}
---
+++ app/src/main/java/io/bytesatwork/skycell/SkycellService.java
@@ -0,0 +1,79 @@
+package io.bytesatwork.skycell;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+public class SkycellService extends Service {
+    private static final String TAG = SkycellService.class.getSimpleName();
+    public static final String CHANNEL_ID = "io.bytesatwork.skycell.CHANNEL_ID";
+    public static final String CHANNEL_NAME = "Channel";
+
+    private final IBinder mBinder = new LocalBinder();
+
+    public class LocalBinder extends Binder {
+        public SkycellService getService() {
+            return SkycellService.this;
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        Log.d(TAG+":"+Utils.getLineNumber(), "onCreate");
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG+":"+Utils.getLineNumber(), "onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.i(TAG+":"+Utils.getLineNumber(), "onBind");
+
+        return mBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG+":"+Utils.getLineNumber(), "onStartCommand");
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(getString(R.string.app_name))
+            .setContentText(getString(R.string.gw_service_running))
+            .setOngoing(true)
+            .setAutoCancel(false)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) { //Android 8.0 SDK 26
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+
+            //Register the channel with the system
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+            notificationManager.notify(startId, notificationBuilder.build());
+        } else {
+            startForeground(startId, notificationBuilder.build());
+        }
+
+        //We want this service to continue running until it is explicitly stopped
+        return START_STICKY;
+    }
+}
