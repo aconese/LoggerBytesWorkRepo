7fc10d7336798e09c98c93cba1362f33d37331a7
Benjamin Imhof <benjamin.imhof@bytesatwork.ch>
2021-07-13 14:30:32 +0200

    Add missing copyright
    Signed-off-by: Benjamin Imhof <benjamin.imhof@bytesatwork.ch>

--- app/src/main/java/io/bytesatwork/skycell/SkyCellApplication.java
+++ app/src/main/java/io/bytesatwork/skycell/SkyCellApplication.java
@@ -1,3 +1,11 @@
+/* Copyright (c) 2021 bytes at work AG. All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * bytes at work AG. ("Confidential Information"). You shall not disclose
+ * such confidential information and shall use it only in accordance with
+ * the terms of the license agreement you entered into with bytes at work AG.
+ */
+
 package io.bytesatwork.skycell;
 
 import io.bytesatwork.skycell.sensor.SensorList;
